ec59b888da6fa8ed7b19894ad901731b2b00a9b3
Antonio Rodriguez <arodriguez588.ar@gmail.com>
2025-02-03 14:47:48 -0800

    comment

--- statify/src/script.js
+++ statify/src/script.js
@@ -27,7 +27,7 @@ export async function redirectToAuthCodeFlow(clientId) {
   params.append("client_id", clientId);
   params.append("response_type", "code");
   params.append("redirect_uri", "http://localhost:5173/callback");
-  params.append("scope", "user-read-private user-read-email user-top-read");
+  params.append("scope", "user-read-private user-read-email user-top-read"); // asks user for authorization to access their info! if you want to access more info, add it here
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
 
