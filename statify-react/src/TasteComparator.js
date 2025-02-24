b7e6957f851d69316e4151460cfe901225296d8f
Aaron-40 <140855821+Aaron-40@users.noreply.github.com>
2025-02-24 15:35:02 -0800

    commenced work on taste comparison usecase
    created TasteComparator function. It will compare user tasts with national statistics
    and see how similar they are.

---
+++ statify-react/src/TasteComparator.js
@@ -0,0 +1,70 @@
+import { Track, getTopTracks } from './getTopTracks';
+
+export async function TasteComparator(token){
+
+    console.log("test");
+    let apiCallString = "https://api.spotify.com/v1/playlists/6UeSakyzhiEt4NB3UAd6NQ?market=US"
+
+    let response = await fetch(apiCallString , {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+
+
+    //If there is an error in fetching the data, this if() block will catch it and print error to the console.
+    if (!response.ok) {
+        console.error("Error fetching national top tracks:", response.status, response.statusText);
+        return null; // Or throw an error
+    }
+
+    //The fetched user top tracks stored in the "response" variable are put in an interpretable JSON format and put inside the tracksJson variable
+    let nationalTracksJson = await response.json();
+
+    //console.log(nationalTracksJson);
+
+    let tracksArray = playlistTrackExtractor(nationalTracksJson);
+
+
+
+
+
+}
+
+
+/* 
+This function is very similar to getTopTracks() in that it returns a array of tracks, rather than doing it based off a user top tracks, this is done by extracting the data from a playlist
+
+*/
+function playlistTrackExtractor(playlistJSON){
+
+
+    let tracksArray = []
+
+
+    for( let i = 0 ;i<playlistJSON.tracks.limit; i++){
+
+        let currTrackJson = playlistJSON.tracks.items[i].track;
+
+
+        let artistArr = []
+        for(let j = 0; j < currTrackJson.artists.length; j++){
+            artistArr.push(currTrackJson.artists[j].name)
+        }
+
+        //Finally, we can create our Track object for this particular track
+        let trackToInsert = new Track(currTrackJson.name,artistArr , currTrackJson.album.images[0].url , currTrackJson.external_urls.spotify,currTrackJson.id )
+        //and insert the track object into the track array
+        tracksArray.push(trackToInsert)
+    }
+
+    console.log(tracksArray)
+
+    return tracksArray;
+
+
+    
+}
+
+
+
+export default TasteComparator
