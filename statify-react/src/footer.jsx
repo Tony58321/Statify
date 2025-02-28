178ad44cbdfc025b2e659267f7509b118b85ade7
granjame <jlg20045@gmail.com>
2025-02-28 13:56:14 -0800

    Fixed footer not anchoring to bottom of the page. Added links to github and help pages.

--- statify-react/src/footer.jsx
+++ statify-react/src/footer.jsx
@@ -1,13 +1,15 @@
+import "./Footer.css"
 
 /*
     A function to return the footer. The footer includes the copyright statement and a link to the help page
 */
+//<button className={""} onClick={() => setPage("Help")}>Help</button>
 export default function Footer(setPage) {
     return (
-    <footer style={{marginTop: "auto"}}>
-        <hr/>
-        <button onClick={() => setPage("Help")}>Help</button>
-        <p style={{color: "white"}}>&copy; Statify 2025</p>
+    <footer>
+        <a href="https://github.com/Tony58321/Statify/blob/main/user_manual.md" target="_blank" className="link-button">Help</a>
+        <a href="https://github.com/Tony58321/Statify/" target="_blank" className="link-button">GitHub</a>
+        <p>&copy; Statify 2025</p>
     </footer>
     );
 }
